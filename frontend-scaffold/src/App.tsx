b10d53f58e661ff4850ba20f00a3ebad09a22188
SamixYasuke <samueladekolu4@gmail.com>
2026-03-25 18:09:42 +0100

    feat: test git commit hook

--- frontend-scaffold/src/App.tsx
+++ frontend-scaffold/src/App.tsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Header from '@/components/layout/Header';
-import Footer from './components/layout/Footer';
-import LandingPage from './features/landing/LandingPage';
-
-// Feature pages - will be implemented in frontend issues
-// import ProfilePage from './features/profile/ProfilePage';
-// import TipPage from './features/tipping/TipPage';
-// import DashboardPage from './features/dashboard/DashboardPage';
-// import LeaderboardPage from './features/leaderboard/LeaderboardPage';
-
-    </BrowserRouter>
-  );
-};
-
-export default App
+const x = 1
